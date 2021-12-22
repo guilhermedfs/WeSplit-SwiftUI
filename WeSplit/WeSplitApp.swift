86810fc84eadcd257c235a5e2bbc4d0bc5d44d78
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2021-12-22 15:56:57 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Guilherme - ília on 22/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
